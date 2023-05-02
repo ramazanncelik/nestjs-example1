c49867dd3521e0c1dee1c2797dfc20a4e2d06852
Ramazan Çelik <73830412+ramazanncelik@users.noreply.github.com>
2023-05-02 19:21:03 +0300

    ResourceServices added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from 'tools/database/database.module';
+import { LibraryModule } from 'libs/libs.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule,LibraryModule],
   controllers: [AppController],
   providers: [AppService],
 })
